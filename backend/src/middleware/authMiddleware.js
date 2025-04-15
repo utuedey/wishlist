b6d3ff23bb3893c057a0b9c126d4e9d4606f0df6
utuedey <utuedeyejosph@gmail.com>
2025-04-15 09:12:55 +0100

    update authMiddleware

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,30 +1,20 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
-exports.verifyToken = async (req, res, next) => {
-
-    // const authHeader = req.headers.authorization;
-    const authHeader = req.headers.authorization;
-    
-    if (!authHeader || !authHeader.startsWith("Bearer ")){
-        return res.status(401).json({
-            message: "Authorization header missing"
-        });
-    }
-    const token = authHeader.split(" ")[1];
+// Middleware to authenticate routes
+const authenticate = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(' ')[1];
     if (!token) {
-        return res.status(403).json({
-            message: "Token is missing"
-        });
+      return res.status(401).json({ success: false, message: 'Not authorized' });
     }
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-        req.user = decoded;
-        next();
-    } catch (error) {
-        return res.status(401).json({
-            message: "Invalid token"
-        });
-    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.userId = decoded.id;
+    next();
+  } catch (err) {
+    res.status(401).json({ success: false, message: 'Not authorized' });
+  }
 };
-     
+
+module.exports = authenticate;
